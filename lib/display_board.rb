99125e6874d3870b4d44dfd3a6f12d7a2bf0dca7
Michael Se-lunani <michael.se-lunani@riarauniversity.ac.ke>
2018-10-16 09:53:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   3.times do
-   puts "   |   |   \n"
-   puts "-----------\n"
-  
+    puts "   |   |   \n"
+    puts "-----------\n"
+  end
 end
 
 display_board
